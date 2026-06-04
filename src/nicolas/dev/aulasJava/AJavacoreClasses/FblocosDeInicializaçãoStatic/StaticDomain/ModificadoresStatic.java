247c42abe67077c7344b2c8b9c79cbca7e43f6cb
Nicolas <nicolassalesalmeida@gmail.com>
2026-06-04 12:52:17 -0300

    Study: Aula de blocos de inicialização e modificadores Static

---
+++ src/nicolas/dev/aulasJava/AJavacoreClasses/FblocosDeInicializaçãoStatic/StaticDomain/ModificadoresStatic.java
@@ -0,0 +1,4 @@
+package nicolas.dev.aulasJava.AJavacoreClasses.FblocosDeInicializaçãoStatic;
+
+public class ModificadoresStatic {
+}
---
+++ src/nicolas/dev/aulasJava/AJavacoreClasses/FblocosDeInicializaçãoStatic/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package nicolas.dev.aulasJava.AJavacoreClasses.FblocosDeInicialização.test;
+
+import nicolas.dev.aulasJava.AJavacoreClasses.FblocosDeInicialização.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime an1 = new Anime();
+
+
+    }
+}
