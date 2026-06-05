2f69ffb553f59aacf2fcafc5bd12254711099494
Nicolas <nicolassalesalmeida@gmail.com>
2026-06-05 12:41:00 -0300

    Study: Initializing blocks statics

---
+++ src/nicolas/dev/aulasJava/AJavacoreClasses/FblocosDeInicializaçãoStatic/StaticDomain/ex1.java
@@ -0,0 +1,4 @@
+package nicolas.dev.aulasJava.AJavacoreClasses.FblocosDeInicializaçãoStatic.StaticDomain;
+
+public class ex1 {
+}
---
+++ src/nicolas/dev/aulasJava/AJavacoreClasses/FblocosDeInicializaçãoStatic/StaticTest/ex2Test.java
@@ -0,0 +1,9 @@
+package nicolas.dev.aulasJava.AJavacoreClasses.FblocosDeInicializaçãoStatic.StaticTest;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        System.out.println(ex2.getParametro("url"));
+
+    }
+}
